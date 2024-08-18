cdb12ae69280f150848e18cebdbaf536c897d072
shiri <irihsmoc@gmail.com>
2024-08-18 16:30:21 +0900

    add: queue, stack

--- src/main/java/projava/part4/TraverseDeepQueue.java
+++ src/main/java/projava/part4/TraverseDeepQueue.java
@@ -1,8 +1,9 @@
 package projava.part4;
 
 import java.util.ArrayDeque;
+import java.util.Queue;
 
-public class TraverseDeep {
+public class TraverseDeepQueue {
     public static void main(String[] args) {
         int[][] map = {
                 {1,1,1,1,1,1,1},
@@ -23,10 +24,10 @@ public class TraverseDeep {
 
     static boolean traverse(int[][] map, int curX, int curY) {
         record Position(int x, int y) {}
-        var stack = new ArrayDeque<Position>();
-        stack.push(new Position(curX, curY));
+        Queue<Position> queue = new ArrayDeque<>();
+        queue.offer(new Position(curX, curY));
 
-        for (Position p; (p = stack.pollFirst()) != null;) {
+        for (Position p; (p = queue.poll()) != null;) {
             switch (map[p.y()][p.x()]) {
                 case 0: break;
                 case 2: return true;
@@ -34,10 +35,10 @@ public class TraverseDeep {
             }
 
             map[p.y()][p.x()] = 3;
-            stack.push(new Position(p.x() + 1, p.y()));
-            stack.push(new Position(p.x() - 1, p.y()));
-            stack.push(new Position(p.x(), p.y() + 1));
-            stack.push(new Position(p.x(), p.y() - 1));
+            queue.offer(new Position(p.x() + 1, p.y()));
+            queue.offer(new Position(p.x() - 1, p.y()));
+            queue.offer(new Position(p.x(), p.y() + 1));
+            queue.offer(new Position(p.x(), p.y() - 1));
         }
 
         return false;
