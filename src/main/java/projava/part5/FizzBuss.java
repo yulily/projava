8c51028dbcadef84c3fd0f959389acc4fdce3af9
shiri <irihsmoc@gmail.com>
2024-08-19 23:24:50 +0900

    add: FizzBuss

---
+++ src/main/java/projava/part5/FizzBuss.java
@@ -0,0 +1,19 @@
+package projava.part5;
+
+public class FizzBuss {
+    public static void main(String[] args) {
+        for (int i = 0; i < 100; i++) {
+            String out = "";
+            if (i % 3 == 0) {
+                out += "Fizz";
+            }
+            if ( i % 5 == 0) {
+                out += "Buzz";
+            }
+            if (i % 3 != 0 && i % 5 !=0) {
+                out += i;
+            }
+            System.out.println(out);
+        }
+    }
+}
